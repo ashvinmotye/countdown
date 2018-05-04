908935353fda25c296ee8ac01d7b8a19d9f57575
ashvintest <motye13@gmail.com>
2018-05-04 21:23:36 +0400

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,31 +1,43 @@
 var tasks = [
   {
-    "title": "Stella",
-    "deadline": "9 May 2018",
-    "category": "personal"
+    title: "Python",
+    deadline: "30 July 2018",
+    category: "learning"
   },
 
   {
-    "title": "O-X",
-    "deadline": "1 May 2018",
-    "category": "games"
+    title: "Machine Learning",
+    deadline: "31 October 2018",
+    category: "learning"
   },
 
   {
-    "title": "Infinity War",
-    "deadline": "27 April 2018",
-    "category": "movies"
+    title: "Networking",
+    deadline: "31 December 2018",
+    category: "learning"
   },
 
   {
-    "title": "Deadpool",
-    "deadline": "16 May 2018",
-    "category": "movies"
+    title: "Stella",
+    deadline: "9 May 2018",
+    category: "personal"
   },
 
   {
-    "title": "400km",
-    "deadline": "22 May 2018",
-    "category": "personal"
+    title: "Infinity War",
+    deadline: "27 April 2018",
+    category: "movies"
+  },
+
+  {
+    title: "Deadpool",
+    deadline: "16 May 2018",
+    category: "movies"
+  },
+
+  {
+    title: "400km",
+    deadline: "22 May 2018",
+    category: "personal"
   }
 ];
